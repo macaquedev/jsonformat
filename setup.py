28250b0f5e824772eea2a93d6200c3e4394a4b01
= <macaquedev@gmail.com>
2021-10-02 22:45:26 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,16 @@ import setuptools
 
 setuptools.setup(
     name='jsonformat',
-    version='0.0.5',
+    version='0.0.6',
     license="MIT",
     entry_points={
         'console_scripts': ['jsonformat=jsonformat:cli'],
     },
+    url="https://github.com/macaquedev/jsonformat",
     author='Alex Pylypenko',
     description='A simple, small command line utility to format json files.',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/x-rst",
     install_requires=[
         'setuptools',
         'pip'

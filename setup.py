c8317e4e1acc71791a442ecb003d86ca0a10ced5
= <macaquedev@gmail.com>
2023-12-25 11:10:53 +0000

    Stuff

--- setup.py
+++ setup.py
@@ -3,13 +3,15 @@ import setuptools
 
 setuptools.setup(
     name='jsonformat',
-    version='0.0.7',
+    version='0.0.11',
     license="MIT",
     entry_points={
-        'console_scripts': ['jsonformat=jsonformat:cli'],
+        'console_scripts': ['jsonformat=jsonformat.jsonformat:cli'],
     },
+    packages=["jsonformat"],
     url="https://github.com/macaquedev/jsonformat",
     author='Alex Pylypenko',
+    author_email="macaquedev@gmail.com",
     description='A simple, small command line utility to format json files.',
     long_description=open('README.md').read(),
     long_description_content_type="text/x-rst",
